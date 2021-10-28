eb05812d0af1b573f27ca6ed0d436e594da123ba
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2021-10-28 21:53:29 +0900

    update clang

--- build_clang_meshlab_assimp.sh
+++ build_clang_meshlab_assimp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-docker build . -f Dockerfile.clang   --build-arg BASE_IMAGE=nvidia/opengl:1.2-glvnd-runtime-ubuntu20.04 -t yoheikakiuchi/clang:20.04
-docker build . -f Dockerfile.meshlab --build-arg BASE_IMAGE=yoheikakiuchi/clang:20.04                   -t tmp_build
-docker build . -f Dockerfile.assimp  --build-arg BASE_IMAGE=tmp_build                                   -t yoheikakiuchi/meshlab_and_assimp:20.04
+
+set -e
+
+docker build . -f Dockerfile.clang   --build-arg BASE_IMAGE=nvidia/opengl:1.2-glvnd-runtime-ubuntu20.04 --pull -t yoheikakiuchi/clang:20.04
+docker build . -f Dockerfile.meshlab --build-arg BASE_IMAGE=yoheikakiuchi/clang:20.04 --build-arg TAG_FOR_BUILD=Meshlab-2021.07 \
+       -t tmp_build
+docker build . -f Dockerfile.assimp  --build-arg BASE_IMAGE=tmp_build --build-arg TAG_FOR_BUILD=v5.0.1 \
+       -t yoheikakiuchi/meshlab_and_assimp:20.04
