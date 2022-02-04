100c6c1aa06c389453a8f3e9dd51c2022f781434
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2022-02-04 20:36:12 +0900

    fix for melodic

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-_ROS_DIST=noetic
+#_ROS_DIST=noetic
+_ROS_DIST=melodic
 
 docker build -f Dockerfile.add_ros.${_ROS_DIST} -t add_ros:${_ROS_DIST} .
 docker build --no-cache -f Dockerfile.novnc --build-arg BASE_IMAGE=add_ros:${_ROS_DIST} -t novnc:${_ROS_DIST} .
